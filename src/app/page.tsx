9bdba68c9b79261ac803e8cffa6257e6fde34328
Guilherme Quincó <guilhermeqcorrea@gmail.com>
2024-09-26 16:12:22 -0400

    feat: creating tables on database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,40 +5,50 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import SignIn from "@/components/sign-in";
 import { auth } from "./auth";
+import { SignOut } from "@/components/sign-out";
+import { revalidatePath } from "next/cache";
+import { eq } from "drizzle-orm";
 
 export default async function Home() {
-  const postsQuery = await database.query.posts.findMany();
-
   const session = await auth();
 
-  if (!session?.user) return null;
+  const postsQuery = await database.query.posts.findMany({
+    with: {
+      user: {
+        name: true,
+      },
+    },
+  });
+
+  console.log(postsQuery);
 
   return (
     <main className="container mx-auto p-12">
-      <SignIn />
+      {session?.user ? <SignOut /> : <SignIn />}
+
       <form
         action={async (formData: FormData) => {
           "use server";
-          await database.insert(posts);
+          await database.insert(posts).values({
+            content: formData.get("content") as string,
+            userId: session?.user?.id!,
+          });
+          revalidatePath("/");
         }}
         className="flex flex-row"
       >
-        <Input />
+        <Input name="content" placeholder="No que você está pensando??" />
         <Button type="submit">Save</Button>
       </form>
-      <Image
-        src={session.user.image}
-        alt="user-image"
-        width={100}
-        height={100}
-        className="rounded-full"
-      />
-      <h1>{session.user.name}</h1>
-      <span>{session.user.email}</span>
 
-      {postsQuery.map((item) => (
-        <span>{item.id}</span>
-      ))}
+      <div className="flex flex-col gap-3">
+        {postsQuery.map((item) => (
+          <div className="flex f">
+            <span>{item.userId}</span>
+            <span>{item.content}</span>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
